d4d1099a9bdfc5e1dd919956a80378c518b1a1c0
Ryan Takatsuka <shotokanryan@gmail.com>
2016-12-13 02:39:10 -0800

    updated comments and documentation strings
    Still need to edit the comments and doc strings to the format of Sphinx
    in order to create proper documentation

--- Dotstars.py
+++ Dotstars.py
@@ -1,101 +1,72 @@
-#!/usr/bin/python
+"""
+Python module to control for APA102C ledstripes (Adafruit Dotstars) on the STM32 Discovery
+Modified from the Wipy code: https://github.com/thilohille/wipy-dotstar
+"""
 
-# Light-painting example for Adafruit Dot Star RGB LED strip.
-# Loads image, displays column-at-a-time on LEDs at a reasonable speed
-# for long exposure photography.
-# See strandtest.py for a much simpler example script.
-# See image-pov.py for a faster persistence-of-vision example.
-
-#from PIL import Image
-#from dotstar import Adafruit_DotStar
-
-##
-## Python module to control for APA102C ledstripes (Adafruit Dotstars) on the Wipy 
-##   
-##
-
+# Import stuff
 from pyb import SPI
 
 class DotStars:
-    "Doc String for this class"    
-    
-    def __init__(self, leds):
-        self.ledcount = leds
-        self.buffersize = self.ledcount * 4
-        self.buffer = bytearray(self.ledcount * 4)
-        self.emptybuffer = bytearray(self.ledcount * 4)
-        for i in range(0, self.buffersize, 4):
-            self.emptybuffer[i] = 0xff
-            self.emptybuffer[i + 1] = 0x0
-            self.emptybuffer[i + 2] = 0x0
-            self.emptybuffer[i + 3] = 0x0   
-        self.startframe = bytes([0x00, 0x00, 0x00, 0x00])
-        self.endframe   = bytes([0xff, 0xff, 0xff, 0xff])
-        self.spi = SPI(1, SPI.MASTER, baudrate=45000000, polarity=0, phase=0,bits=8, firstbit=SPI.MSB)
-        self.clearleds()
+	"""
+	This class creates a Dotstars object that can control the Dotstar LEDs over SPI. 
 
-       
-    #init empty self.buffer
-    def clearleds(self):
-        self.buffer = self.emptybuffer[:]
+	"""
+	
+	# Initialize the class the SPI communication
+	def __init__(self, leds):
+		self.ledcount = leds
+		# create a buffer
+		self.buffersize = self.ledcount * 4
+		self.buffer = bytearray(self.ledcount * 4)
+		self.emptybuffer = bytearray(self.ledcount * 4)
+		for i in range(0, self.buffersize, 4):
+			self.emptybuffer[i] = 0xff
+			self.emptybuffer[i + 1] = 0x0
+			self.emptybuffer[i + 2] = 0x0
+			self.emptybuffer[i + 3] = 0x0   
+		# Start frame and endframe for the SPI communication (end frame is not needed)
+		self.startframe = bytes([0x00, 0x00, 0x00, 0x00])
+		self.endframe   = bytes([0xff, 0xff, 0xff, 0xff])
+		# initialize SPI (needs to be at 45 MHz in order to maximize the speed.  This is the limiting factor for the system's speed)
+		self.spi = SPI(1, SPI.MASTER, baudrate=45000000, polarity=0, phase=0,bits=8, firstbit=SPI.MSB)
+		self.clearleds()
 
-    @micropython.native    
-    def setled(self, led, red=0, green=0, blue=0):
-#        if (led > self.ledcount):
-#            led=led % self.ledcount
-#        
-#        if (led < 0):
-#            led = self.ledcount + led
-        
-        offset = led * 4
-        self.buffer[offset] = 255  # equals a 1 or 0
-        self.buffer[offset + 1] = blue
-        self.buffer[offset + 2] = green
-        self.buffer[offset + 3] = red
+	   
+	# empties the buffer and clears the LEDs
+	def clearleds(self):
+		self.buffer = self.emptybuffer[:]
 
-    def send(self):
-        #self.spi.write(self.startframe + self.buffer + self.endframe)
-        self.spi.send(self.startframe + self.buffer)
-        
-        
+	# Compile this method into native code for the STM32 Discovery in order to optimize the speed of the program
+	@micropython.native    
+	# sets the LED with a specific red, green, and blue value
+	def setled(self, led, red=0, green=0, blue=0):
+		# Set the offset for the bytes to be sent over SPI
+		offset = led * 4
+		self.buffer[offset] = 255  # equals a 1 or 0
+		self.buffer[offset + 1] = blue
+		self.buffer[offset + 2] = green
+		self.buffer[offset + 3] = red
+
+	# Send the data over SPI (this takes about 100 us with a baud rate of 45 MHz)
+	def send(self):
+		# send the start frame and buffer (no need for the endframe)
+		self.spi.send(self.startframe + self.buffer)
+		
+# Used for debugging purposes		
 if __name__ == '__main__': 
-    Dotty = DotStars(72)
-    Dotty.send()
-    bri = 1
-    numpixels = 30          # Number of LEDs in strip
-#    filename  = "hello.png" # Image file to load
+	Dotty = DotStars(72)
+	Dotty.send()
+	bri = 1
+	numpixels = 72
+	width     = 20
+	height    = 20
 
-	# Here's how to control the strip from any two GPIO pins:
-	#datapin   = 23
-	#clockpin  = 24
-	#strip     = Adafruit_DotStar(numpixels, datapin, clockpin)
-
-	#strip.begin()           # Initialize pins for output
-
-	# Load image in RGB format and get dimensions:
-#    print ("Loading...")
-#    img       = Image.open(filename).convert("RGB")
-#    pixels    = img.load()
-    width     = 20
-    height    = 20
-#    print ("%dx%d pixels" % img.size)
-
-	#if(height > strip.numPixels()): height = strip.numPixels()
-
-	# Calculate gamma correction table, makes mid-range colors look 'right':
-#    gamma = bytearray(256)
-#    for i in range(256):
-#        gamma[i] = int(pow(float(i) / 255.0, 2.7) * 255.0 + 0.5)
-
-#    print ("Displaying...")
-    while True:                              # Loop forever
-
-    	for x in range(width):           # For each column of image...
-         for y in range(height):  # For each pixel in column...
-#             value = pixels[x, y]   # Read pixel in image
-             Dotty.setled(y, # Set pixel in strip
-             test.red[y*width+x],     # Gamma-corrected red
-             test.green[y*width+x],     # Gamma-corrected green
-             test.blue[y*width+x])     # Gamma-corrected blue
-             Dotty.send()             # Refresh LED strip
-	     #pyb.udelay(50)
+	while True:								# Loop forever
+		for x in range(width):				# For each column of image...
+			for y in range(height):			# For each pixel in column...
+				Dotty.setled(y,				# Set pixel in strip
+				test.red[y*width+x],		# red value
+				test.green[y*width+x],		# green value
+				test.blue[y*width+x])		# blue value
+				Dotty.send()				# Refresh LED strip
+			pyb.udelay(50)
--- main.py
+++ main.py
@@ -1,97 +1,91 @@
 """
 Created on Tue Dec  6 23:02:00 2016
 
-@author: mecha27
+@author: Ryan Takatsuka
 """
+
+# Import stuff
 from Dotstars import DotStars
 import byte_picture
 import pyb
-import sys
 import array
 
+# create a DotStars class with 72 total LEDs
 Dotty = DotStars(72)
 Dotty.send()
-bri = 1
-width     = 15
-height    = 24
-led_refresh_time = 900
+# Specify the width and height of the image that will be imported
+width     = 15 # [pixels]
+height    = 24 # [pixels]
+led_refresh_time = 900 # [microseconds]
 
-# Converting the data into byte arrays was slower???
+# Converting the pixel list data into byte arrays
 red_pixels = bytearray(byte_picture.red)
 green_pixels = bytearray(byte_picture.green)
 blue_pixels = bytearray(byte_picture.blue)
 
+# Time arrays for calculating the maximum refresh rate of the microcontroller
 times_array = array.array('L', [0]*(width*height))
-times_outer = array.array('L', [0]*(width*height))
-times_row = array.array('L', [0]*(width*height))
-
+times_for_SPIsend = array.array('L', [0]*(width*height))
 diff_times_array = array.array('L', [0]*1000)
-diff_times_outer = array.array('L', [0]*1000)
 diff_times_row = array.array('L', [0]*1000)
 
-#red_pixels = byte_picture.red
-#green_pixels = byte_picture.green
-#blue_pixels = byte_picture.blue
+# Variable that will be set during the ISR to determine timing
+rotation_time = 0
 
+# Handler method for the hall effect sensor
 def hall_ISR(pin):
-    print("printing")
-    
-extint = pyb.ExtInt(pyb.Pin.cpu.A0, pyb.ExtInt.IRQ_FALLING, pyb.Pin.PULL_UP, hall_ISR)
+	global rotation_time
+	rotation_time = millis()
+	
+# setup the interrupt on the PA0 pin on the STM32 Discovery board
+interrupts = pyb.ExtInt(pyb.Pin.cpu.A0, pyb.ExtInt.IRQ_FALLING, pyb.Pin.PULL_UP, hall_ISR)
 
+# Method for finding the difference between elements in an array
 def array_diff(in_array):
-    diff_array = array.array('L',[0]*1000)
-    for index, component in enumerate(in_array):
-        if index<len(in_array)-1:
-            diff_array[index] = (in_array[index+1] - in_array[index])
-    return diff_array
-            
-
+	diff_array = array.array('L',[0]*1000)
+	for index, component in enumerate(in_array):
+		if index<len(in_array)-1:
+			diff_array[index] = (in_array[index+1] - in_array[index])
+	return diff_array
+			
+# Method for printing the times recorded during the main loop
 def timerTest():
-        
-    diff_times_array = array_diff(times_array)
-#    diff_times_row = array_diff(times_row)
-    
-    # print the times it takes to refresh each led
-    for row in range(height):
-        for refresh_delay in range(width):
-            print(str(diff_times_array[row*width+refresh_delay]), end=",")
-        print()
-    
-    # print the time it takes to update each column of leds    
-    print('\n')
-    for row in range(height):
-        print(str(diff_times_row[row]), end=",")
-        
-    # print the time it takes to to the function Dotty.send()
-    print('\n\n')
-    for row in range(height):
-        print(str(times_outer[row]), end=",")
-      
-    print('\n')
-        
-#    sys.exit()
+	# Find the difference of the array of times
+	diff_times_array = array_diff(times_array)
+	
+	# print the times it takes to refresh each led
+	for row in range(height):
+		for refresh_delay in range(width):
+			print(str(diff_times_array[row*width+refresh_delay]), end=",")
+		print()
+		
+	# print the time it takes to to the function Dotty.send()
+	print('\n')
+	for row in range(height):
+		print(str(times_for_SPIsend[row]), end=",")
+	print('\n')
 
-
+# Main loop that updates the LEDs
 while True:         
-    for x in range(width):           # For each column of image...
-        for y in range(height):  # For each pixel in column...
-            Dotty.setled(y, # Set pixel in strip
-            red_pixels[y*width+x],     # Gamma-corrected red
-            green_pixels[y*width+x],     # Gamma-corrected green
-            blue_pixels[y*width+x])     # Gamma-corrected blue
-            
-            # Dotty.send takes the most amount of time
-            start = pyb.micros()
-            Dotty.send()             # Refresh LED strip
-            times_outer[y+x*width] = pyb.elapsed_micros(start)
-            
-            times_array[y+x*width] = pyb.micros()
-#            times_array[y*width+x] = pyb.elapsed_micros(start)         
+	for x in range(width):			# For each column of image...
+		for y in range(height):		# For each pixel in column...
+			Dotty.setled(y,			# Set pixel in strip
+			red_pixels[y*width+x],	# red value
+			green_pixels[y*width+x],	# green value
+			blue_pixels[y*width+x])		# blue value
+			
+			# Dotty.send takes the most amount of time
+			start = pyb.micros()
+			Dotty.send()             # Refresh LED strip
+			
+			# Record the time it took to refresh the LEDs and to send the data over SPI
+			times_for_SPIsend[y+x*width] = pyb.elapsed_micros(start)
+			times_array[y*width+x] = pyb.micros()        
 
-#            if pyb.elapsed_micros(start) > led_refresh_time:
-#                print ("refresh time is too fast. Error on pixel #" + str(y*width+x))
-#            while pyb.elapsed_micros(start) < led_refresh_time:
-#                pass
+			# Wait a specified time to match the rotational speed of the system
+			if pyb.elapsed_micros(start) > led_refresh_time:
+				print ("refresh time is too fast. Error on pixel #" + str(y*width+x))
+			while pyb.elapsed_micros(start) < led_refresh_time:
+				pass
 
-        times_row[x] = pyb.micros()
-#    timerTest()
+#	timerTest() - Can run this method to print the timing of the system
